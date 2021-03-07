6dc9c46fad2340115dce9220b8ea9ae67a60b5ff
softpaper95 <minho100227@likelion.org>
2021-03-07 17:01:07 +0900

    question phone number

--- 1.연습장.py
+++ 1.연습장.py
@@ -1,15 +1,3 @@
-import math
-n = int(input("n의 값을 입력하시오:"))
-m = int(input("m의 값을 입력하시오:"))
-numbers = [True for i in range(n+1)]
-
-
-for j in range(2, int(math.sqrt(n))+1):
-    if numbers[j] == True:
-        k = 2
-        while j * k <= n:
-            numbers[j * k] = False
-            k += 1
-for v in range(m, n):
-    if numbers[v]:
-        print(v)
+a = "93423412"
+A = [i for i in a]
+print(A)
---
+++ 프로그래머스/해쉬/전화번호 목록.py
@@ -0,0 +1,44 @@
+# 시도1
+# def solution(phone_book):
+#     for i in range(1,len(phone_book)):
+#         if phone_book[0] in phone_book[i]:
+#             answer = False
+#             break
+#         else:
+#             answer = True
+#     return answer
+
+# 시도2
+# a = [i for i in phone_book[0]]
+# for k in (1, len(phone_book)):
+#     count = 0
+#     b = [j for j in phone_book[k]]
+#     for x,y in zip(a,b):
+#         if x == y:
+#             count += 1
+#     if count == len(a):
+#         answer = False
+#         break
+#     else:
+#         answer = True
+
+
+phone_book = ["119", "97674223", "1195524421"]
+# 정렬을 하면 phone_book[0]의 값과 가장가까운 값이 순서대로 정렬되므로, 이 값의 앞과 뒤만 확인해주면 된다.
+# ex) 정렬후 -> [119, 1195524421, 97674223] 이므로 zip으로 앞뒤 확인만 해주면 된다.
+
+phone_book.sort()
+answer = True
+for x,y in zip(phone_book, phone_book[1:]):
+    if x in y or y in x:
+        answer = False
+print(answer)
+
+# 참고할것
+# https://ychae-leah.tistory.com/47
+
+
+
+
+
+
